67b43a049109d3996558d3eb9284d8a985810554
Sofiia Hoppova <sofiahoppova@gmail.com>
2026-01-06 19:46:56 +0200

    Migrate main entry to TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import App from "./App.jsx";
 
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
