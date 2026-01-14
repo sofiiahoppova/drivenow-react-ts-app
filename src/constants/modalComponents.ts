76f09f35f21eeb7c1ba3a621565bb12f35dc6eae
Sofiia Hoppova <sofiahoppova@gmail.com>
2026-01-14 15:52:52 +0200

    Type modal components constants

---
+++ src/constants/modalComponents.ts
@@ -0,0 +1,13 @@
+import ReviewsList from "@/components/catalog/ReviewsList/ReviewsList";
+import SearchBarModal from "@/components/catalog/SearshBar/SearchBarModal";
+import LogOutModal from "@/components/account/LogOutModal/LogOutModal";
+import AuthModal from "@/components/auth/AuthModal/AuthModal";
+import DeleteBookingModal from "@/components/booking/DeleteBookingModal/DeleteBookingModal";
+
+export const modalComponents = {
+  ReviewsList,
+  SearchBarModal,
+  LogOutModal,
+  AuthModal,
+  DeleteBookingModal,
+};
